8dff65ac2764f2c64b4f1d5e99805d30355aae22
Catalina Erlich <cerlich@pr-d5.kzn.21-school.ru>
2021-04-06 05:04:44 +0300

    1

---
+++ setup.sh
@@ -0,0 +1,6 @@
+minikube start --vm-driver=virtualbox
+minikube dashboard
+minikube addons enable metallb
+eval $(minikube docker-env)
+cd srcs/nginx && docker build -t nginx_image . && kubectl apply -f nginx.yaml
+cd .. && kubectl apply -f ips.yaml
