e26456284c57c68a00572041e5790e783f410267
Catalina Erlich <cerlich@ge-b5.kzn.21-school.ru>
2021-04-07 01:46:42 +0300

    2

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ minikube start --vm-driver=virtualbox
 minikube dashboard
 minikube addons enable metallb
 eval $(minikube docker-env)
-cd srcs/nginx && docker build -t nginx_image . && kubectl apply -f nginx.yaml
-cd .. && kubectl apply -f ips.yaml
+#cd srcs/nginx && docker build -t nginx_image . && kubectl apply -f nginx.yaml
+#cd .. && kubectl apply -f ips.yaml
---
+++ srcs/mysql/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mariadb-install-db -u root
+mysqld -u root
+
+echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+sleep 5
+
+#exec /usr/bin/mysqld --user=mysql --console
