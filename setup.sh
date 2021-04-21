dc9006377bc7b93cfa8dd074f7837b4999f23255
Catalina Erlich <cerlich@ge-n6.kzn.21-school.ru>
2021-04-21 22:10:03 +0300

    add ftps

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ minikube start --vm-driver=virtualbox --cpus=4 --memory=4G --disk-size=50G
 #minikube dashboard
 
 eval $(minikube docker-env)
+docker pull metallb/speaker:v0.8.2
+docker pull metallb/controller:v0.8.2
 minikube addons enable metallb
 kubectl apply -f srcs/ips.yaml
 
@@ -24,5 +26,9 @@ kubectl apply -f srcs/wordpress/wp.yaml
 #phpmyadmin
 docker build -t phpmyadmin_image srcs/phpmyadmin
 kubectl apply -f srcs/phpmyadmin/pma.yaml
+
+#ftps
+docker build -t ftps_image srcs/ftps
+kubectl apply -f srcs/ftps/ftps.yaml
 #sleep 2
 minikube dashboard
