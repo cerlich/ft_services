f44364279fedebc98e8287c793c9e5741af95c27
Catalina Erlich <cerlich@un-l5.kzn.21-school.ru>
2021-04-22 23:51:50 +0300

    fix pma

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -6,7 +6,7 @@ mysqld -u root & sleep 5
 mysql -u root -e "CREATE DATABASE wordpress;"
 
 # copy wp database
-# mysql -u root wordpress < wordpress.sql
+ mysql -u root wordpress < wordpress.sql
 
 mysql -u root -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin' WITH GRANT OPTION; USE wordpress;"
